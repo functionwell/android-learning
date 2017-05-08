081d11349b8d0df88bca44069587684b215a817e
FangKe <FangKe>
2017-05-08 09:38:06 +0800

    GeoQuiz

---
+++ GeoQuiz/app/src/main/java/me/funcl/android/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package me.funcl.android.geoquiz;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mAnswerTrue = answerTrue;
+        mTextResId = textResId;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
